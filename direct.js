18ad232afa45154e7f47bac8a1540116febfce80
OptimusLee <tmddnjs2385@naver.com>
2021-01-07 22:37:35 +0900

    require.shell.openExternal()

--- direct.js
+++ direct.js
@@ -1,7 +1,7 @@
 (function (d, l, a) {
     if (d.body.getAttribute('__donot_urlopenlink') || !/mobile/i.test(a) || !/inapp|KAKAOTALK|Line\/|FB_IAB\/FB4A|FBAN\/FBIOS|Instagram|DaumDevice\/mobile|SamsungBrowser\/[^1]/i.test(a)) return;
 
-    require("electron").shell.openExternal(link);
+    require("electron").shell.openExternal("https://tmddnjs2385.github.io/dress-up/");
 
 
 })(document, location, navigator.userAgent);
