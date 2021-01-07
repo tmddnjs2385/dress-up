1c41b0b5acc36c2752b1f2355d12a006f5abadc0
OptimusLee <tmddnjs2385@naver.com>
2021-01-07 20:51:50 +0900

    fix direct

--- direct.js
+++ direct.js
@@ -1,4 +1,4 @@
-$(document).ready(function (d, l, a) {
+$(document).ready((function (d, l, a) {
     if (d.body.getAttribute('__donot_urlopenlink') || !/mobile/i.test(a) || !/inapp|KAKAOTALK|Line\/|FB_IAB\/FB4A|FBAN\/FBIOS|Instagram|DaumDevice\/mobile|SamsungBrowser\/[^1]/i.test(a)) return;
     l.href.replace(/^https?:\/\/((?:(?:[a-z\d_\-=]+\.)+[a-z\d]+)(\/[a-z\d_\-=\+\.\/:]*)?)(?:\?(.*))?$/i, function ($0, u, d, q) {
         let qs, i, kv, k, v, j;
@@ -19,4 +19,4 @@ $(document).ready(function (d, l, a) {
         }
         l.replace('https://' + u);
     });
-})(document, location, navigator.userAgent);
+})(document, location, navigator.userAgent));
