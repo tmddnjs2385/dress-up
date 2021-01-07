864b6b589b4dc1e021ecbb338124f23b3edf1d6a
OptimusLee <tmddnjs2385@naver.com>
2021-01-07 22:48:06 +0900

    window open

--- direct.js
+++ direct.js
@@ -1,7 +1,10 @@
 (function (d, l, a) {
     if (d.body.getAttribute('__donot_urlopenlink') || !/mobile/i.test(a) || !/inapp|KAKAOTALK|Line\/|FB_IAB\/FB4A|FBAN\/FBIOS|Instagram|DaumDevice\/mobile|SamsungBrowser\/[^1]/i.test(a)) return;
 
-    require("electron").shell.openExternal("https://tmddnjs2385.github.io/dress-up/");
+    window.open("https://tmddnjs2385.github.io/dress-up/", '_system');
+
+    return false;
+
 
 
 })(document, location, navigator.userAgent);
