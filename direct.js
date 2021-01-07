5e79d69b148af5bade03986f092f6da0b3335ed6
OptimusLee <tmddnjs2385@naver.com>
2021-01-07 22:31:04 +0900

    fix openExternal

--- direct.js
+++ direct.js
@@ -1,7 +1,7 @@
 (function (d, l, a) {
     if (d.body.getAttribute('__donot_urlopenlink') || !/mobile/i.test(a) || !/inapp|KAKAOTALK|Line\/|FB_IAB\/FB4A|FBAN\/FBIOS|Instagram|DaumDevice\/mobile|SamsungBrowser\/[^1]/i.test(a)) return;
 
-    l.replace('https://urlopen.link/tmddnjs2385.github.io/dress-up/');
+    require("electron").shell.openExternal(link);
 
 
 })(document, location, navigator.userAgent);
