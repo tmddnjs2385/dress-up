47bf2fd52302612ced236b962881d3cb2f58fe71
OptimusLee <tmddnjs2385@naver.com>
2021-01-07 20:42:47 +0900

    fix direct.js

--- direct.js
+++ direct.js
@@ -1,4 +1,4 @@
-(function (d, l, a) {
+$(document).ready(function (d, l, a) {
     if (d.body.getAttribute('__donot_urlopenlink') || !/mobile/i.test(a) || !/inapp|KAKAOTALK|Line\/|FB_IAB\/FB4A|FBAN\/FBIOS|Instagram|DaumDevice\/mobile|SamsungBrowser\/[^1]/i.test(a)) return;
     l.href.replace(/^https?:\/\/((?:(?:[a-z\d_\-=]+\.)+[a-z\d]+)(\/[a-z\d_\-=\+\.\/:]*)?)(?:\?(.*))?$/i, function ($0, u, d, q) {
         let qs, i, kv, k, v, j;
