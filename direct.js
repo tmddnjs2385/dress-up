941e2f458a5c3d5d53e3e57649f90f8a5a7ba8ca
OptimusLee <tmddnjs2385@naver.com>
2021-01-08 10:26:36 +0900

    fix direct window

--- direct.js
+++ direct.js
@@ -1,10 +1,10 @@
-(function (d, l, a) {
-    if (d.body.getAttribute('__donot_urlopenlink') || !/mobile/i.test(a) || !/inapp|KAKAOTALK|Line\/|FB_IAB\/FB4A|FBAN\/FBIOS|Instagram|DaumDevice\/mobile|SamsungBrowser\/[^1]/i.test(a)) return;
+(function (a) {
+    if (!/mobile/i.test(a) || !/inapp|KAKAOTALK|Line\/|FB_IAB\/FB4A|FBAN\/FBIOS|Instagram|DaumDevice\/mobile|SamsungBrowser\/[^1]/i.test(a)) {
+        return;
+    } else {
+        window.open("https://tmddnjs2385.github.io/dress-up/", '_system');
+    }
 
-    window.open("https://tmddnjs2385.github.io/dress-up/", '_system');
-
-    return false;
 
 
-
-})(document, location, navigator.userAgent);
+})(navigator.userAgent);
