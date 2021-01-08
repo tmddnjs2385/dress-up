7ce7a13f624d4e9927b81cccd8eb38894b7f72b2
OptimusLee <tmddnjs2385@naver.com>
2021-01-08 10:40:54 +0900

    remove direct js

--- direct.js
+++ direct.js
@@ -1,10 +1,10 @@
-(function (a) {
-    if (!/mobile/i.test(a) || !/inapp|KAKAOTALK|Line\/|FB_IAB\/FB4A|FBAN\/FBIOS|Instagram|DaumDevice\/mobile|SamsungBrowser\/[^1]/i.test(a)) {
-        return;
-    } else {
-        window.open("https://tmddnjs2385.github.io/dress-up/", '_system');
-    }
+
+if (!/mobile/i.test(a) || !/inapp|KAKAOTALK|Line\/|FB_IAB\/FB4A|FBAN\/FBIOS|Instagram|DaumDevice\/mobile|SamsungBrowser\/[^1]/i.test(a)) {
+    return;
+} else {
+    window.open("https://tmddnjs2385.github.io/dress-up/", '_system');
+}
 
 
 
-})(navigator.userAgent);
+
