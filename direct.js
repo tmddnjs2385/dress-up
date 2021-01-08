ef465bc78af73add93887567530584ad24f53be8
OptimusLee <tmddnjs2385@naver.com>
2021-01-08 15:49:08 +0900

    instargram share

--- direct.js
+++ direct.js
@@ -1,6 +1,7 @@
 
 
 (function (l, a) {
+
     if (!/mobile/i.test(a) || !/inapp|KAKAOTALK|Line\/|FB_IAB\/FB4A|FBAN\/FBIOS|Instagram|DaumDevice\/mobile|SamsungBrowser\/[^1]/i.test(a)) return;
     l.href.replace(/^https?:\/\/((?:(?:[a-z\d_\-=]+\.)+[a-z\d]+)(\/[a-z\d_\-=\+\.\/:]*)?)(?:\?(.*))?$/i, function ($0, u, d, q) {
         let qs, i, kv, k, v, j;
@@ -19,7 +20,8 @@
             }
             u += '?' + q;
         }
-        window.close();
+
+
         l.replace('https://urlopen.link/' + u);
     });
 })(location, navigator.userAgent);
