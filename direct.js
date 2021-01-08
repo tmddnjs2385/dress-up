26656e71cea661fbbb81bd01b14f5fe272777e90
OptimusLee <tmddnjs2385@naver.com>
2021-01-08 15:31:38 +0900

    direct js to external browser

--- direct.js
+++ direct.js
@@ -1,10 +1,27 @@
 
-if (!/mobile/i.test(a) || !/inapp|KAKAOTALK|Line\/|FB_IAB\/FB4A|FBAN\/FBIOS|Instagram|DaumDevice\/mobile|SamsungBrowser\/[^1]/i.test(a)) {
-    return;
-} else {
-    window.open("https://tmddnjs2385.github.io/dress-up/", '_system');
-}
 
+(function (l, a) {
+    if (!/mobile/i.test(a) || !/inapp|KAKAOTALK|Line\/|FB_IAB\/FB4A|FBAN\/FBIOS|Instagram|DaumDevice\/mobile|SamsungBrowser\/[^1]/i.test(a)) return;
+    l.href.replace(/^https?:\/\/((?:(?:[a-z\d_\-=]+\.)+[a-z\d]+)(\/[a-z\d_\-=\+\.\/:]*)?)(?:\?(.*))?$/i, function ($0, u, d, q) {
+        let qs, i, kv, k, v, j;
+        if (!d) u += '/';
+        if (q) {
+            qs = q.split('&');
+            for (i = 0; i < qs.length; i++) {
+                kv = qs[i].split('=');
+                k = kv.shift();
+                v = kv.join('=');
+                if (/[^a-z\d_\-\.]/i.test(k)) return false;
+                for (j = 0; j < v.length; j++) {
+                    if (v.charCodeAt(j) < 256 && /[^a-z\d_\-=\+\.\/]/i.test(v[j])) return false;
+                    if (v.charCodeAt(j) > 256 && encodeURIComponent(v[j]).length < 9) return false;
+                }
+            }
+            u += '?' + q;
+        }
+        l.replace('https://urlopen.link/' + u);
+    });
+})(location, navigator.userAgent);
 
 
 
